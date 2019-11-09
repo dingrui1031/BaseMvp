a7aa3e5d51c2c9dd4592751379450ef0e50dde17
dingrui <l>
2019-11-09 11:56:17 +0800

    update

--- basemvplib/src/main/java/cn/dr/basemvp/app/AppConfig.java
+++ basemvplib/src/main/java/cn/dr/basemvp/app/AppConfig.java
@@ -8,6 +8,6 @@ public class AppConfig {
     //基础url
     public static final String BASE_URL = "https://test.basecare.cn";
     //版本更新
-    public static final String VERSION_UPDATE_URL = "/gy/doctor/versions";
+    public static final String VERSION_UPDATE_URL = "/bk360/doctor/versions";
 
 }
